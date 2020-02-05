3b1b61faf2265fd8ee668379249b6a6bbf4b1e9c
Parkash-Sharma <parkashsharma33@gmail.com>
2020-02-06 00:42:32 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -76,7 +76,7 @@ document.addEventListener('deviceready',onDeviceReady, false);
 
 var MyCampusApp = {
     config : {
-        tenant : "Unifyedpreprod",
+        tenant : "brenauunifyed",
         serverUrl : "https://kryptos.kryptosmobile.com",
         tenantFolder : function(device, tenant) {
             if(device.platform == 'Android') {
